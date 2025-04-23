66cdccbcc9ccae81f617d2c44b13dad7f7ebda60
bryan.qurniawan <bryan.qurniawan@weekendinc.com>
2025-04-23 14:31:01 +0700

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -491,6 +491,7 @@ const Home = () => {
                       </div>
                     </button>
                     <Link
+                      to={`/works/${project.id}`}
                       className="top-[35px] absolute z-0 w-full"
                       style={{ height: "calc(100vh - 202px)" }}
                     >
@@ -672,32 +673,34 @@ const Home = () => {
           </div>
         </FadeInSection>
       </div>
-      {hoveredProject !== null && deviceType === "desktop" (
-        <div
-          className="fixed pointer-events-none z-[15] flex items-center justify-center"
-          style={{
-            left: mousePosition.x,
-            top: mousePosition.y,
-            transform: "translate(-50%, -50%)",
-            width: "200px",
-            height: "120px",
-            borderRadius: "50%",
-            mixBlendMode: "difference",
-            fontWeight: "bold",
-            color: "white",
-            overflow: "hidden",
-          }}
-        >
-          <div className="marquee-container overflow-hidden w-[80%]">
-            <div className="marquee-text whitespace-nowrap animate-marquee">
-              {projects[hoveredProject]?.title} •{" "}
-              {projects[hoveredProject]?.client} •{" "}
-              {projects[hoveredProject]?.title} •{" "}
-              {projects[hoveredProject]?.client} •{" "}
+      {hoveredProject !== null &&
+        deviceType ===
+          "desktop"(
+            <div
+              className="fixed pointer-events-none z-[15] flex items-center justify-center"
+              style={{
+                left: mousePosition.x,
+                top: mousePosition.y,
+                transform: "translate(-50%, -50%)",
+                width: "200px",
+                height: "120px",
+                borderRadius: "50%",
+                mixBlendMode: "difference",
+                fontWeight: "bold",
+                color: "white",
+                overflow: "hidden",
+              }}
+            >
+              <div className="marquee-container overflow-hidden w-[80%]">
+                <div className="marquee-text whitespace-nowrap animate-marquee">
+                  {projects[hoveredProject]?.title} •{" "}
+                  {projects[hoveredProject]?.client} •{" "}
+                  {projects[hoveredProject]?.title} •{" "}
+                  {projects[hoveredProject]?.client} •{" "}
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      )}
+          )}
     </div>
   );
 };
